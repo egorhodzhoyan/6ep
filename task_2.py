a0bf9ca39ee1c5d663ba96825bec13265a6d1c36
Egor <egor.hodzhoyan@ro.ru>
2025-01-28 19:56:28 +0300

    redact

--- task_2.py
+++ task_2.py
@@ -1,1 +1,12 @@
-number = int(input('Введите число: '))
+number = int(input('Введите ваше число: '))
+amount = 0
+if number > 0:
+    while number > 0:
+        number //= 10
+        amount += 1
+    print('Количество цифр в числе:', amount)
+elif number == 0:
+    amount += 1
+    print('Количество цифр в числе:', amount)
+else:
+    print('Недопустимое число!')
