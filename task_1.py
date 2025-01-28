020f0ab873dbb4832028562190ae0924c87a0799
Egor <egor.hodzhoyan@ro.ru>
2025-01-28 19:24:25 +0300

    redact

--- task_1.py
+++ task_1.py
@@ -1,1 +1,7 @@
-number = int(input('Введите число: '))
+number = int(input('Введите ваше число: '))
+a = 0
+while a < number:
+    a += 1
+    cube = a ** 3
+    print('Ваш результат:')
+    print(a, '** 3 =', cube)
