2eaead4123aa97be1f68a61e9505c2a665d327b5
Egor <egor.hodzhoyan@ro.ru>
2025-01-28 20:30:04 +0300

    redact

---
+++ task_4.py
@@ -0,0 +1,13 @@
+deposit = int(input('Введите сумму на вкладе в банке: '))
+percent = int(input('Введите под какой процент вложены средства: '))
+limit_deposit = int(input('Введите порог вклада: '))
+years = 0
+while deposit <= limit_deposit:
+    count = (deposit * percent // 100)
+    years += 1
+    if years > 1:
+        deposit += count
+    print(years, 'год.', deposit, '+', percent, '% =', deposit + count)
+    if deposit + count >= limit_deposit:
+        break
+print('Количество лет для достижения порога:', years)
